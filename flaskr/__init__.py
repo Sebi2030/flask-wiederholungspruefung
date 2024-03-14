90a8925e41a446fe72a4f13cfc7d013bc34cec8b
Sebi2030 <s_nieme@yahoo.de>
2024-03-14 20:29:51 +0100

    change to the sqlalchemy URI

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,7 +10,8 @@ def create_app(test_config=None):
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
         # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.instance_path, "database.db"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
     if test_config is None:
