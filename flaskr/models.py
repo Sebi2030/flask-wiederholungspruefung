83b36df45c46208da4c35ba092f6dc9429022861
Sebi2030 <s_nieme@yahoo.de>
2024-03-17 17:57:45 +0100

    Refactor User class and add posts relationship

--- flaskr/models.py
+++ flaskr/models.py
@@ -18,10 +18,12 @@ author_id = db.Column(db.Integer, db.ForeignKey('user.id',ondelete="CASCADE"), n
 author = db.relationship('User', back_populates='posts')
  
 
-class User
+class User(db.Model,UserMixin):
 id = db.Column(db.Integer, primary_key=True)
 username = db.Column(db.String(80), unique=True, nullable=False)
 password = db.Column(db.String(120), nullable=False)
+
+posts = db.relationship('Post', back_populates='author', passive_deletes=True)
 
 
 class Like
