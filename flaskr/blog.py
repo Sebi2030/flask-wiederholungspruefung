2094a49bec86c0790901b8237fe1c5803623978a
Sebi2030 <s_nieme@yahoo.de>
2024-03-18 00:02:53 +0100

    Refactor index route to use Post.query.all()

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -17,12 +17,7 @@ bp = Blueprint("blog", __name__)
 @bp.route("/")
 def index():
     """Show all the posts, most recent first."""
-    db = get_db()
-    posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username"
-        " FROM post p JOIN user u ON p.author_id = u.id"
-        " ORDER BY created DESC"
-    ).fetchall()
+    posts = Post.query.all()
     return render_template("blog/index.html", posts=posts)
 
 
