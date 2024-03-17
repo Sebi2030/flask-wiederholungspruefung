0e29fd6600163e33040da4c7ddc9fa38491463a7
Sebi2030 <s_nieme@yahoo.de>
2024-03-17 18:10:46 +0100

    Refactor models.py to improve code readability

--- flaskr/models.py
+++ flaskr/models.py
@@ -7,35 +7,35 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 class Post(db.Model):
-id = db.Column(db.Integer, primary_key=True)
-created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
-title = db.Column(db.String(120), nullable=False)
-body = db.Column(db.Text, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
 
-likes = db.relationship('Like', back_populates='post', passive_deletes=True)
+    likes = db.relationship('Like', back_populates='post', passive_deletes=True)
 
-author_id = db.Column(db.Integer, db.ForeignKey('user.id',ondelete="CASCADE"), nullable=False)
-author = db.relationship('User', back_populates='posts')
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id',ondelete="CASCADE"), nullable=False)
+    author = db.relationship('User', back_populates='posts')
  
 
 class User(db.Model,UserMixin):
-id = db.Column(db.Integer, primary_key=True)
-username = db.Column(db.String(80), unique=True, nullable=False)
-password = db.Column(db.String(120), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
-posts = db.relationship('Post', back_populates='author', passive_deletes=True)
-likes = db.relationship('Like', back_populates='author', passive_deletes=True)
+    posts = db.relationship('Post', back_populates='author', passive_deletes=True)
+    likes = db.relationship('Like', back_populates='author', passive_deletes=True)
 
 
 class Like(db.Model):
-id = db.Column(db.Integer, primary_key=True)
-created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
 
-author_id = db.Column(db.Integer, db.ForeignKey(
+    author_id = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
     author = db.relationship('User', back_populates='likes')
 
-post_id = db.Column(db.Integer, db.ForeignKey(
+    post_id = db.Column(db.Integer, db.ForeignKey(
         'post.id', ondelete="CASCADE"), nullable=False)
     post = db.relationship('Post', back_populates='likes')
 
