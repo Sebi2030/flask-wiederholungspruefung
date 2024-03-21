1dd73bd6188d8bf69db43e220129eb032448dc8f
Sebi2030 <s_nieme@yahoo.de>
2024-03-21 21:28:02 +0100

    Add validation for patch fields in PostUpdateSchema

--- flaskr/schemas.py
+++ flaskr/schemas.py
@@ -11,4 +11,8 @@ class PostUpdateSchema(Schema):
     title = fields.Str()
     body = fields.Str()
     author_id = fields.Int()
-    
+
+    @validates_schema
+    def validate_patch_fields(self, data, **kwargs):
+        if not any(data.values()):
+            raise ValidationError("At least one field must be provided for update.")
