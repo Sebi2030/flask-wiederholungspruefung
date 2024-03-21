9a88675fce3e6f6982624341b51d2af4a47e91c8
Sebi2030 <s_nieme@yahoo.de>
2024-03-21 20:52:04 +0100

    Add PostSchema to flaskr/schemas.py

--- flaskr/schemas.py
+++ flaskr/schemas.py
@@ -1,1 +1,8 @@
 from marshmallow import Schema, fields, validates_schema, ValidationError
+
+class PostSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    body = fields.Str(required=True)
+    created = fields.DateTime(dump_only=True)
+    author_id = fields.Int(required=True)
