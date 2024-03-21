28e07a352dc4882a590e1f37a6ec5a9c6ce63e5a
Sebi2030 <s_nieme@yahoo.de>
2024-03-21 23:55:55 +0100

    Refactor API routes and add blueprint for blog posts

--- flaskr/api_routes.py
+++ flaskr/api_routes.py
@@ -1,5 +1,8 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from .models import Post,db, User
-from .schemas import PostSchema,PostUpdateSchema
+from .models import Post, db, User
+from .schemas import PostSchema, PostUpdateSchema
 
+blp = Blueprint("posts", "posts", url_prefix="/posts", description="Operations on blog posts")
+
+@blp.route("/")
