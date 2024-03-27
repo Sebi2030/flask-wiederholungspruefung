a2f14e04bf9754c05f473a994e18a8fc60597eee
Sebi2030 <s_nieme@yahoo.de>
2024-03-27 22:33:15 +0100

    indentation errors have been fixed

--- flaskr/api_routes.py
+++ flaskr/api_routes.py
@@ -14,7 +14,6 @@ class Posts(MethodView):
     
     @blp.arguments(PostSchema)
     @blp.response(201, PostSchema)
-
     def post(self, new_data):
         """Add a new post"""
         if 'author_id' in new_data and not User.query.filter_by(id=new_data['author_id']).first():
@@ -22,39 +21,39 @@ class Posts(MethodView):
         new_post = Post(**new_data)
         db.session.add(new_post)
         db.session.commit()
-        return new
+        return new_post
     
     @blp.route("/<post_id>")
     class PostsById(MethodView):
-    @blp.response(200, PostSchema)
-    def get(self, post_id):
-       """Get post by ID"""
-       post = Post.query.filter_by(id=post_id).first()
-       if post:
-            return post
-        abort(404, message="Post not found")
+        @blp.response(200, PostSchema)
+        def get(self, post_id):
+            """Get post by ID"""
+            post = Post.query.filter_by(id=post_id).first()
+            if post:
+                return post
+            abort(404, message="Post not found")
        
-     @blp.arguments(PostUpdateSchema)
-     @blp.response(200, PostSchema)
-     def patch(self, update_data, post_id):
-       """Update existing post"""
-       post = Post.query.filter_by(id=post_id).first()
-       if 'author_id' in update_data and not User.query.filter_by(id=update_data['author_id']).first():
-            abort(400, message="author_id doesn't exists")
-        if post:
-            for key, value in update_data.items():
-                setattr(post, key, value)
-            # Commit the changes to the database
-            db.session.commit()
-            return post
-        abort(404, message="Post not found")
+        @blp.arguments(PostUpdateSchema)
+        @blp.response(200, PostSchema)
+        def patch(self, update_data, post_id):
+            """Update existing post"""
+            post = Post.query.filter_by(id=post_id).first()
+            if 'author_id' in update_data and not User.query.filter_by(id=update_data['author_id']).first():
+                abort(400, message="author_id doesn't exists")
+            if post:
+                for key, value in update_data.items():
+                    setattr(post, key, value)
+                # Commit the changes to the database
+                db.session.commit()
+                return post
+            abort(404, message="Post not found")
        
-    @blp.response(204)
-    def delete(self, post_id):
-        """Delete post"""
-        post = Post.query.filter_by(id=post_id).first()
-        if post:
-            Post.query.filter_by(id=post_id).delete()
-            db.session.commit()
-            return {"message": "Post deleted."}
-        abort(404, message="Post not found.")
+        @blp.response(204)
+        def delete(self, post_id):
+            """Delete post"""
+            post = Post.query.filter_by(id=post_id).first()
+            if post:
+                Post.query.filter_by(id=post_id).delete()
+                db.session.commit()
+                return {"message": "Post deleted."}
+            abort(404, message="Post not found.")
