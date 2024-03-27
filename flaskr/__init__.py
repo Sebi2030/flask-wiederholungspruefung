f680072a118f57924124db8e0b1aafd153f8dbd6
Sebi2030 <s_nieme@yahoo.de>
2024-03-27 22:56:15 +0100

    Remove duplicate blueprints and update OpenAPI Swagger UI URL

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -41,13 +41,6 @@ def create_app(test_config=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     api = Api(app)
 
-    # apply the blueprints to the app
-    from . import auth
-    from . import blog
-
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
-
     # register the database commands
     from .models import db,login_manager
 
